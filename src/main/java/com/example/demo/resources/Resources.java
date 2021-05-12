63875ce29e4957900adf67b081d1cdedc123c90b
wesley estevão <wesleyestevao333@gmail.com>
2021-05-12 17:33:57 -0300

    DTO

--- src/main/java/com/example/demo/resources/Resources.java
+++ src/main/java/com/example/demo/resources/Resources.java
@@ -1,6 +1,7 @@
 package com.example.demo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.UserDTO;
 import com.example.demo.entities.User;
 import com.example.demo.services.UserServices;
 
@@ -19,10 +21,11 @@ public class Resources {
 	private UserServices us;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User> list = us.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
 	}
 }
