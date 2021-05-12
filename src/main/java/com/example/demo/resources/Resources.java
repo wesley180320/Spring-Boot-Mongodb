aabd84ffbad94e6e910b2b6383cd470a1ef15b15
wesley estevão <wesleyestevao333@gmail.com>
2021-05-12 16:46:56 -0300

    adicionando banco mongodb

--- src/main/java/com/example/demo/resources/Resources.java
+++ src/main/java/com/example/demo/resources/Resources.java
@@ -1,29 +1,28 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserServices;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class Resources {
 
-	@RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private UserServices us;
+	
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
 		
-		List<User> list = new ArrayList<>();
-		User maria = new User(1, "maria", "maria@gmail.com");
-		User wesley = new User(1, "wesley", "wesley@gmail.com");
-
-		list.addAll(Arrays.asList(maria, wesley));
-
+		List<User> list = us.findAll();
+		
 		return ResponseEntity.ok().body(list);
 	}
 }
